fc3ba2476a52b288a78f544bf6ee88b929321309
Efrain Lima Miranda <efrain70@gmail.com>
2018-05-06 16:27:26 +0200

    Added unittest combine coverage all

--- tests/unit/neudataload/tranformer/test_combine.py
+++ tests/unit/neudataload/tranformer/test_combine.py
@@ -117,3 +117,34 @@ class TestCombinations(object):
         output = pipe.fit_transform(profiles.data_frame)
 
         assert sorted(list(output.columns)) == ['DTI_FA', 'DTI_L1']
+
+    def test_no_df_simple(self, datafiles):
+
+        path = os.path.join(str(datafiles), 'small_data')
+        filename = 'profiles.xlsx'
+
+        profiles = NeuProfiles(profiles_path=path, profiles_filename=filename)
+
+        assert profiles.data_frame is None
+
+        profiles.load()
+
+        assert profiles.data_frame is not None
+
+        pipe = Pipeline([
+            ('combining', CombineMatrixTransformer(
+                columns=[0, 1],
+                column_name='combined')),
+        ])
+        output = pipe.fit_transform(profiles.data_frame.values)
+
+        import numpy as np
+
+        assert (output[0][0] == np.asarray(
+            [[11, 12, 13], [14, 15, 16], [17, 18, 19]])).all()
+
+        assert (output[0][1] == np.asarray(
+            [[1, 2, 3, ], [4, 5, 6, ], [7, 8, 9, ]])).all()
+
+        assert (output[0][2] == np.asarray(
+            [[6, 7, 8, ], [9, 10, 11], [12, 13, 14]])).all()
